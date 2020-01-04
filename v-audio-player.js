e3d287211990220591a9620d553f3647d5c240e2
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-01-04 11:23:37 +0100

    Add proof

--- v-audio-player.js
+++ v-audio-player.js
@@ -13,6 +13,8 @@ class AudioPlayer
   
   playSound(soundfile)
   {
+    // Uses the HTMLAudioElement interface, which provides access to the
+    // properties of <audio> elements, as well as manipulate them
     let audio = new Audio(soundfile);
     audio.play();
   }
